a8d91d71de5be0340c51650966493f3158244e38
Amir BEN SLIMANE <amir.benslimane@gmail.com>
2020-02-24 23:10:11 +0100

    create skeleton main page with header router and footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-
-
+import { MaterialModule } from '../material.module';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, HomeComponent, LoginComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ]
 })
 export class CoreModule { }
